11d710ba0c16462ba4cc9db4b0762206d3b43a0d
Guy Marshall <guy@cslgroupservices.co.uk>
2022-08-18 12:29:43 +0100

    Add prompts for 2 choices - need to implement logic for each

--- src/com/marshall/guy/Main.java
+++ src/com/marshall/guy/Main.java
@@ -3,26 +3,12 @@ package com.marshall.guy;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-/*
-    USE BIGINTEGER INSTEAD
- */
-
 public class Main {
 
-//    public static int collatz(int number) {
     public static BigInteger collatz(BigInteger number) {
-//        int counter = 0;
         BigInteger counter = BigInteger.ZERO;
-        // if even, divide by 2
-        // else times by 3 and add 1
-//        while (number > 1) {
-//            counter++;
-//            if (number % 2 == 0) {
-//                number = number / 2;
-//            } else {
-//                number = (3 * number) + 1;
-//            }
-//        }
+//        if even, divide by 2
+//        else times by 3 and add 1
         while (number.compareTo(BigInteger.ONE) > 0) {
             counter = counter.add(BigInteger.ONE);
             if (number.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
@@ -36,33 +22,40 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.print("Enter a positive integer: ");
         Scanner scanner = new Scanner(System.in);
-//        int userInput = scanner.nextInt();
-        BigInteger userInput = scanner.nextBigInteger();
 
-//        int maxNumber = 0;
-//        int maxSteps = 0;
-        BigInteger maxNumber = BigInteger.ZERO;
-        BigInteger maxSteps = BigInteger.ZERO;
+        System.out.println("Do you want to calculate:");
+        System.out.println("1 - Maximum number of steps for a range up to the input");
+        System.out.println("2 - Number of steps and maximum number reached for the input");
+        System.out.print("Enter your choice (1 OR 2): ");
+        int choice = scanner.nextInt();
 
-//        for (int i = 1; i <= userInput; i++) {
-//            if (maxSteps < collatz(i)) {
-//                maxSteps = collatz(i);
-//                maxNumber = i;
-//            }
-//
-////            System.out.printf("%d: %d%n", i, collatz(i));
-//        }
-
-        BigInteger counter = BigInteger.ONE;
-        for (; counter.compareTo(userInput) <= 0; counter = counter.add(BigInteger.ONE)) {
-            if (maxSteps.compareTo(collatz(counter)) < 0) {
-                maxSteps = collatz(counter);
-                maxNumber = counter;
-            }
+        switch (choice) {
+            case 1:
+                //do loop through numbers up to range and do collatz on those
+                break;
+            case 2:
+                //do collatz for number entered and return number of steps and max number reached
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+                System.exit(0);
         }
 
-        System.out.printf("%d had the maximum number of steps at %d", maxNumber, maxSteps);
+//        System.out.print("Enter a positive integer: ");
+//        BigInteger userInput = scanner.nextBigInteger();
+//
+//        BigInteger maxNumber = BigInteger.ZERO;
+//        BigInteger maxSteps = BigInteger.ZERO;
+//
+//        BigInteger counter = BigInteger.ONE;
+//        for (; counter.compareTo(userInput) <= 0; counter = counter.add(BigInteger.ONE)) {
+//            if (maxSteps.compareTo(collatz(counter)) < 0) {
+//                maxSteps = collatz(counter);
+//                maxNumber = counter;
+//            }
+//        }
+//
+//        System.out.printf("%d had the maximum number of steps at %d", maxNumber, maxSteps);
     }
 }
