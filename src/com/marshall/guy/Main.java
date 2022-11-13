840093c026e682df09077158977f56daccfba411
Guy Marshall <guymarshall9@gmail.com>
2022-11-13 21:56:41 +0000

    Simplify codebase significantly

--- src/com/marshall/guy/Main.java
+++ src/com/marshall/guy/Main.java
@@ -7,21 +7,6 @@ import java.util.Scanner;
 //        else times by 3 and add 1
 
 public class Main {
-
-    //implement collatz function using int (for performance)
-    public static int collatz(int number) {
-        int counter = 0;
-        while (number > 1) {
-            counter++;
-            if (number % 2 == 0) {
-                number /= 2;
-            } else {
-                number = (number * 3) + 1;
-            }
-        }
-
-        return counter;
-    }
     public static BigInteger collatz_bigint(BigInteger number) {
         BigInteger counter = BigInteger.ZERO;
         while (number.compareTo(BigInteger.ONE) > 0) {
@@ -39,35 +24,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Do you want to calculate:");
-        System.out.println("1 - Maximum number of steps for a range up to the input");
-        System.out.println("2 - Number of steps and maximum number reached for the input");
-        System.out.print("Enter your choice (1 OR 2): ");
-        BigInteger choice = scanner.nextBigInteger();
-
         System.out.print("Enter a positive integer: ");
         BigInteger userInput = scanner.nextBigInteger();
-        BigInteger maxNumber = BigInteger.ZERO;
-        BigInteger maxSteps = BigInteger.ZERO;
-
-        if (choice.equals(BigInteger.valueOf(1))) {
-            //do loop through numbers up to range and do collatz on those
-            BigInteger counter = BigInteger.ONE;
-            for (; counter.compareTo(userInput) <= 0; counter = counter.add(BigInteger.ONE)) {
-                BigInteger result = collatz_bigint(counter);
-                if (maxSteps.compareTo(result) < 0) {
-                    maxSteps = result;
-                    maxNumber = counter;
-                }
-            }
-
-            System.out.printf("%d had the maximum number of steps at %d", maxNumber, maxSteps);
-        } else if (choice.equals(BigInteger.valueOf(2))) {
-            //do collatz for number entered and return number of steps and max number reached
-            System.out.printf("%d had %d steps%n", userInput, collatz_bigint(userInput));
-        } else {
-            System.out.println("Invalid option. Please try again.");
-            System.exit(0);
-        }
+        System.out.printf("%d had %d steps%n", userInput, collatz_bigint(userInput));
     }
 }
